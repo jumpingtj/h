a6376ade1442098bdab2dbe43fb246ffb76196ee
jumpingtj <58107984+jumpingtj@users.noreply.github.com>
2020-11-11 16:26:36 +0000

    Create js.js

---
+++ js.js
@@ -0,0 +1,4 @@
+var libraries = {
+    'processingjs':'https://cdn.jsdelivr.net/processing.js/1.4.8/processing.min.js',
+    'slideshow':'https://cdn.jsdelivr.net/gh/pamelafox/ka-slideshow-example@master/slideshow.js'
+};
